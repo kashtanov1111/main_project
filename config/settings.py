aca12d93c047672ae3c122094bd2023b959ffd19
unknown <1kashtanov1111@gmail.com>
2021-11-15 21:41:25 +0500

    ch6

--- config/settings.py
+++ config/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     #3rd party
     'crispy_forms',
+    'allauth',
+    'allauth.account',
 
     #Local
     'accounts',
@@ -140,7 +143,20 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ########
 AUTH_USER_MODEL = 'accounts.CustomUser'
 TIME_ZONE = 'Asia/Yekaterinburg'
-LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'home'
+ACCOUNT_LOGOUT_REDIRECT = 'home'
+SITE_ID = 1
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_UNIQUE_EMAIL = True
