d93c6f7b7f51428f580468e2779aa1fe7639f7f1
unknown <1kashtanov1111@gmail.com>
2021-12-05 17:22:13 +0500

    ch

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from .forms import CustomUserCreationForm #, UserProfileinfoForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.conf import settings
 
 from .models import CustomUser
 from .forms import UserProfileForm, CustomUserChangeForm
@@ -13,6 +14,11 @@ def userprofile(request):
         profile_form = CustomUserChangeForm(request.POST, request.FILES, instance=request.user)
        
         if profile_form.is_valid():
+            #if settings.USE_S3:
+            #    upload = Upload
+            
+            
+            
             profile_form.save()
             #profile_form = profile_form.save(commit=False)  
             #profile_form.user = request.user
