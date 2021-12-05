fa2c491e2759dd78943c0816583b7a3b185d6c5a
unknown <1kashtanov1111@gmail.com>
2021-12-05 12:10:41 +0500

    ch16

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin-2281953/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('user/', include('accounts.urls')),
     path('polls/', include('polls.urls')),
